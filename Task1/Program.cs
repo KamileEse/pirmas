d854b796ea5dddb2269760a350c8be44090ed768
KamileEse <eselinaitek@gmail.com>
2021-10-18 20:25:29 +0300

    aa

---
+++ Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task1
+{
+    partial class MainClass
+    {
+        //1. Programa turi paprašyti varototojo suvesti savo amžių
+        //2. Jeigu mažiau nei 18, išspausdinti, kad vartotojas dar negali vairuoti ar pirkti alkoholio
+        //3. Jeigu daugiau nei 18 bet mažiau nei 20, kad gali vairuoti, bet negali pirkti alko.. 
+        //4. Jeigu daugiau nei 20, kad gali ir vairuoti ir pirkti alko..
+
+        public static void Main(string[] args)
+        {
+            AgeChecker age = new AgeChecker();
+            int amzius = age.Nuskaitymas();
+            Possibilities galimybe = age.ageEvaluate(amzius);
+            IsvestiRezultata(galimybe);
+            Console.ReadLine();
+        }
+
+        public static void IsvestiRezultata(Possibilities galimybe)
+        {
+            switch (galimybe)
+            {
+                case Possibilities.nieko:
+                    Console.WriteLine("Nieko negalima");
+                    break;
+                case Possibilities.vairuoti:
+                    Console.WriteLine("Vairuoti gali, pirkti negali");
+                    break;
+                case Possibilities.vairuotiGerti:
+                    Console.WriteLine("Daryk ka nori");
+                    break;
+            }
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task3
+{
+    class MainClass
+    {
+        //1. Programa turi priimti nerobotą kiekį skaičių iki kol varotojas įveda 0 (nulis)
+        //2. Varotojui suvedus 0 (nulį), programa išspausdina visus iki tol
+        //suvestus skaičius vienoje eilutėje.
+
+        public static void Main(string[] args)
+        {
+            IkiNulio vedam = new IkiNulio();
+
+            Console.ReadLine();
+        }
+    }
+}
